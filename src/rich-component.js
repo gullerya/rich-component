276dc864cca0305a7618ebe5de40c616bba50bbf
gullerya <gullerya@gmail.com>
2019-12-24 11:27:41 +0200

    filled the API tests

--- src/rich-component.js
+++ src/rich-component.js
@@ -24,41 +24,33 @@ export {
 	fetchTemplate
 };
 
-async function initComponent(tagName, componentClass) {
-	if (componentHTMLs[tagName]) {
-		throw new Error(`component MAY NOT be initialized more than once, '${tagName}' is attempted to be initialized more than once`);
-	}
-	if (!componentClass || !(componentClass.prototype instanceof ComponentBase)) {
-		throw new Error(`invalid component class of '${tagName}'; MUST NOT be null and MUST be an instance of ComponentBase`);
-	}
-	if ((!(TEMPLATE_PROPERTY in componentClass) && !(HTML_URL_PROPERTY in componentClass)) ||
-		(TEMPLATE_PROPERTY in componentClass && HTML_URL_PROPERTY in componentClass)) {
-		throw new Error(`'${tagName}' MUST implement either static getter of '${HTML_URL_PROPERTY}' property returning component's HTML path, or static getter of '${TEMPLATE_PROPERTY}' property returning a template`);
-	}
+async function initComponent(tag, type) {
+	validataTag(tag);
+	validateType(tag, type);
 
 	let template;
-	if (TEMPLATE_PROPERTY in componentClass) {
-		template = componentClass[TEMPLATE_PROPERTY];
+	if (TEMPLATE_PROPERTY in type) {
+		template = type[TEMPLATE_PROPERTY];
 		if (template.nodeName !== 'TEMPLATE') {
-			throw new Error(`'${tagName}' provided invalid template (${template})`);
+			throw new Error(`'${tag}' provided invalid template (${template})`);
 		}
 	} else {
-		const templateUrl = componentClass[HTML_URL_PROPERTY];
+		const templateUrl = type[HTML_URL_PROPERTY];
 		if (!templateUrl || typeof templateUrl !== 'string') {
-			throw new Error(`'${tagName}' provided invalid HTML URL (${templateUrl})`);
+			throw new Error(`'${tag}' provided invalid HTML URL (${templateUrl})`);
 		}
 
 		const templateRaw = await fetchTemplate(templateUrl);
 		if (!templateRaw) {
-			throw new Error(`failed to init template of '${tagName}' from '${templateUrl}'`)
+			throw new Error(`failed to init template of '${tag}' from '${templateUrl}'`)
 		}
 
 		template = document.createElement('template');
 		template.innerHTML = templateRaw;
 	}
 
-	componentHTMLs[tagName] = template;
-	customElements.define(tagName, componentClass);
+	componentHTMLs[tag] = template;
+	customElements.define(tag, type);
 }
 
 async function fetchTemplate(templateUrl) {
@@ -79,4 +71,23 @@ async function fetchTemplate(templateUrl) {
 		console.error(`failed to fetch HTML template from '${templateUrl}', status ${htmlResponse.status}`);
 	}
 	return result;
+}
+
+function validataTag(tag) {
+	if (!tag || typeof tag !== 'string' || !/^[a-z]+(-[a-z]+)*-[a-z]+$/.test(tag)) {
+		throw new Error(`invalid element's tag/name: ${tag}`);
+	}
+	if (componentHTMLs[tag]) {
+		throw new Error(`'${tag}' MAY NOT be initialized more than once`);
+	}
+}
+
+function validateType(tag, type) {
+	if (!type || !(type.prototype instanceof ComponentBase)) {
+		throw new Error(`invalid class for '${tag}'; MUST NOT be null and MUST be an instance of ComponentBase`);
+	}
+	if ((!(TEMPLATE_PROPERTY in type) && !(HTML_URL_PROPERTY in type)) ||
+		(TEMPLATE_PROPERTY in type && HTML_URL_PROPERTY in type)) {
+		throw new Error(`'${tag}' MUST implement either static getter of '${HTML_URL_PROPERTY}' property returning component's HTML path, or static getter of '${TEMPLATE_PROPERTY}' property returning a template`);
+	}
 }
--- tests/test-apis.js
+++ tests/test-apis.js
@@ -4,7 +4,64 @@ import * as RC from '../../dist/rich-component.js';
 const suite = createSuite({ name: 'Testing rich-component APIs' });
 
 suite.runTest({ name: 'API present basic' }, test => {
-	test.assertEqual('function', typeof RC.ComponentBase);
 	test.assertEqual('function', typeof RC.initComponent);
+	test.assertEqual('function', typeof RC.ComponentBase);
 	test.assertEqual('function', typeof RC.fetchTemplate);
+});
+
+suite.runTest({ name: 'initComponent API - undefined tag', expectError: 'invalid element\'s tag\/name:' }, async () => {
+	await RC.initComponent();
+});
+
+suite.runTest({ name: 'initComponent API - null tag', expectError: 'invalid element\'s tag\/name:' }, async () => {
+	await RC.initComponent(null);
+});
+
+suite.runTest({ name: 'initComponent API - non-string tag', expectError: 'invalid element\'s tag\/name:' }, async () => {
+	await RC.initComponent(function () { });
+});
+
+suite.runTest({ name: 'initComponent API - empty-string tag', expectError: 'invalid element\'s tag\/name:' }, async () => {
+	await RC.initComponent('');
+});
+
+suite.runTest({ name: 'initComponent API - invalid custom element name', expectError: 'invalid element\'s tag\/name:' }, async () => {
+	await RC.initComponent('some');
+});
+
+suite.runTest({ name: 'initComponent API - undefined class', expectError: 'invalid class for' }, async () => {
+	await RC.initComponent('a-a-a');
+});
+
+suite.runTest({ name: 'initComponent API - null class', expectError: 'invalid class for' }, async () => {
+	await RC.initComponent('a-a-a', null);
+});
+
+suite.runTest({ name: 'initComponent API - non-function class', expectError: 'invalid class for' }, async () => {
+	await RC.initComponent('a-a-a', {});
+});
+
+suite.runTest({ name: 'initComponent API - not ComponentBase extending class', expectError: 'invalid class for' }, async () => {
+	const c = class Some { };
+	await RC.initComponent('a-a-a', c);
+});
+
+suite.runTest({
+	name: 'initComponent API - not implementing template/htmlUrl class', expectError: 'MUST implement either static getter of'
+}, async () => {
+	const c = class Some extends RC.ComponentBase { };
+	await RC.initComponent('a-a-a', c);
+});
+
+suite.runTest({ name: 'initComponent API - implementing both template/htmlUrl class', expectError: 'MUST implement either static getter of' }, async () => {
+	const c = class Some extends RC.ComponentBase { static get template() { return ''; } static get htmlUrl() { return ''; } };
+	await RC.initComponent('a-a-a', c);
+});
+
+suite.runTest({ name: 'initComponent API - duplicate definition', expectError: 'MAY NOT be initialized more than once' }, async () => {
+	const c1 = class Some extends RC.ComponentBase { static get template() { return document.createElement('template'); } };
+	await RC.initComponent('a-a-a', c1);
+
+	const c2 = class Some extends RC.ComponentBase { static get template() { return document.createElement('template'); } };
+	await RC.initComponent('a-a-a', c2);
 });
